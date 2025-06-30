100ba4aa37a2f16fd2ca4971001e03640fe49b74
Tanya7777 <115713465+Tanya7777@users.noreply.github.com>
2025-06-30 15:58:57 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (98.31%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-         int i=0;
-         //int count=0;
-        int n = nums.size();
-        int c= n/2;
-        for(i=0; i<n;i++){
-             int count=0;
-            for(int j=0; j<n; j++){
-                if(nums[i]==nums[j]){
-                    count++;
+         int candit = nums[0];
+         int count=1;
+         for(int i=0; i<nums.size(); i++){
+            if(nums[i]== candit){
+                count ++;
+            }else{
+                count--;
             }
+
+            if(count==0){
+                candit = nums[i];
+                count=1;
             }
-            if(count >c){
-                return nums[i];
-            }   
-        }
-        return 0;  
-    }
+         }
+            return candit;
+         }
+        
 };
